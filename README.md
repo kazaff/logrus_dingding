680d233af07bb08a492616349a4e29df2c5defa3
kazaff <edisondik@gmail.com>
2018-06-05 05:03:37 -0400

    no message

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # logrus_dingding
 dingding robot notification
+
+[doc](https://open-doc.dingtalk.com/docs/doc.htm?spm=a219a.7629140.0.0.lmydCN&treeId=257&articleId=105735&docType=1)
+
+code:
+```
+package main
+
+import (
+	"github.com/sirupsen/logrus"
+	ding "github.com/kazaff/logrus_dingding"
+)
+
+func main(){
+	log := logrus.New()
+	log.Hooks.Add(ding.NewDingRobot("app name", "robot hook link"))
+
+	log.WithField("a","b").Fatal("hola!")
+}
+```
---
+++ dingding.go
@@ -0,0 +1,70 @@
+package logrus_dingding
+
+import (
+	"github.com/sirupsen/logrus"
+	"encoding/json"
+	"net/http"
+	"bytes"
+	"io/ioutil"
+	"strings"
+	"errors"
+)
+
+type DingHook struct {
+	AppName string
+	Robot string
+}
+
+func NewDingRobot(appname string, host string)*DingHook{
+	return &DingHook{
+		AppName: appname,
+		Robot: host,
+	}
+}
+
+func (hook *DingHook) Fire(entry *logrus.Entry) error{
+
+	fields, _:= json.Marshal(entry.Data)
+	fields, _ = json.Marshal(string(fields[:]))	// 为了转义
+	tmp := strings.TrimRight(string(fields[1:]), "\"")	// 截断前后的双引号
+
+	message := `{"msgtype": "markdown","markdown": {"title": "`+entry.Level.String()+`","text": "#### ` + entry.Level.String() + `\n ![screenshot](http://lorempixel.com/400/200/cats?t=`+time.Now().Format(time.RFC850)+`)\n\n `+ entry.Message + `\n\n `+tmp+`"}}`
+	jsonStr := []byte(message)
+	req, err := http.NewRequest("POST", hook.Robot, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	var ddResponce struct {
+		Errcode int
+		Errmsg string
+	}
+	err = json.Unmarshal(body, &ddResponce)
+	if err != nil {
+		return err
+	}
+
+	if ddResponce.Errcode != 0 {
+		return errors.New(ddResponce.Errmsg)
+	}
+
+	return nil
+}
+
+func (hook *DingHook) Levels()[]logrus.Level{
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+	}
+}
